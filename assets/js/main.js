d092dcdd72d37c7d1fcca3bf04101f549e1c5920
rlcoder <rocksnaiom@gmail.com>
2017-06-30 20:02:18 -0300

    function rowAddTotable() and editRow() to get Id in Table row.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,45 +7,17 @@ Description: Crud simples com Javascript
 Version: 1.0
 =================================================*/
 //Global var
-var _row	 		 = null,
-		_nextId		 = 1,
+var _nextId		 = 1,
 		_activedId = 0;
-
-//function to add row in table
-if ($("#crudTable tbody")== 0){
-function rowAdd(){
-		$("#crudTable").append("<tbody></tbody>");
-	}
-
-		$("#crudTable tbody").append(
-		"<tr>"+
-			"<td>000</td>" +
-			"<td>Not Product</td>"+
-			"<td>Null</td>"+
-			"<td><div class=\"group-actions\">"+
-			"<button class=\"btn btn-info btn-action\"><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>"+
-			"<button class=\"btn btn-danger btn-action\"><i class=\"fa fa-times-circle\" aria-hidden=\"true\"></i></button>"+
-			"</div></td>"+
-		"</tr>"
-	);
-}
 
 //function to add row in table
 function rowAddToTable(){
 	if ($("#crudTable tbody")== 0){
 		$("#crudTable").append("<tbody></tbody>");
-	}
-		$("#crudTable tbody").append(
-		"<tr>"+
-			"<td>001</td>" +
-			"<td>"+ $("#productName").val()+"</td>"+
-			"<td>"+ $("#productDetail").val()+"</td>"+
-			"<td><div class=\"group-actions\">"+
-			"<button class=\"btn btn-info btn-action\" onclick=\"editRow(this);\"><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>"+
-			"<button class=\"btn btn-danger btn-action\" onclick=\"deleteRow(this);\"><i class=\"fa fa-times-circle\" aria-hidden=\"true\"></i></button>"+
-			"</div></td>"+
-		"</tr>"
-	);
+	}	//Add an id in the method that passes as param param id the incremented id
+		$("#crudTable tbody").append(buildTableRow(_nextId));
+		//increment id
+		_nextId +=1;
 }
 //function to add row in table where id= item clicked
 function buildTableRow(id){
@@ -55,11 +27,13 @@ function buildTableRow(id){
 		"<td>"+ $("#productName").val()+"</td>"+
 		"<td>"+ $("#productDetail").val()+"</td>"+
 		"<td><div class=\"group-actions\">"+
-		"<button class=\"btn btn-info btn-action\" onclick=\"editRow(this);\"><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>"+
-		"<button class=\"btn btn-danger btn-action\" onclick=\"deleteRow(this);\"><i class=\"fa fa-times-circle\" aria-hidden=\"true\"></i></button>"+
+		"<button class=\"btn btn-info btn-action\" onclick=\"editRow(this);\"" +
+			"data-id= \""+ id +"\"><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></button>"+
+		"<button class=\"btn btn-danger btn-action\" onclick=\"deleteRow(this);\" "+
+			"data-id=\"" + id +"\"><i class=\"fa fa-times-circle\" aria-hidden=\"true\"></i></button>"+
 		"</div></td>"+
 	"</tr>";
-	return row;
+	return rowTable;
 }
 
 //clear inputs
@@ -70,14 +44,19 @@ function formClear(){
 
 //function to delete table row
 function deleteRow(get_btnID){
+	//select the clicked eleme
 	$(get_btnID).parent().parent().parent("tr").remove();
 }
 
 //function to edit table row in form
 function editRow(get_btnID){
-	_row = $(get_btnID).parent().parent().parent("tr");
-	var cols = _row.children("td");
+	var row  = $(get_btnID).parent().parent().parent("tr");
+	var cols = row.children("td");
 	console.log(cols);
+	console.log(cols[3]);
+	_activedId = $($(cols[3]).find(".group-actions").children("button")[0]).data("id");
+	console.log("active id");
+	console.log(_activedId);
 	$("#productName").val($(cols[1]).text());
 	$("#productDetail").val($(cols[2]).text());
 
@@ -85,12 +64,15 @@ function editRow(get_btnID){
 }
 
 //function to update row table
-function tableUpdateRow(){
-	console.log(_row);
+function tableUpdateRow(id){
+		console.log("Tabela de adicionar linha");
+	console.log(_activedId);
+	var row = $(".group-actions button[data-id='"+ id +"']");
+	console.log(row);
 	//Adiciona linha na tabela
-	$(_row).after(buildTableRow());
+	$(row).after(buildTableRow());
 	//remove old row
-	$(_row).remove();
+	$(row).remove();
 	formClear();
 	$("#updateButton").text("Add Product");
 }
@@ -99,7 +81,7 @@ function tableUpdateRow(){
 function tableUpdate(){
 	if ($("#productName").val() != null && $("#productName").val()!=''){
 		if($("#updateButton").text() == "Update"){
-			tableUpdateRow();
+			tableUpdateRow(_activedId)
 		} else {
 			rowAddToTable();
 		}
@@ -109,6 +91,6 @@ function tableUpdate(){
 }
 	$(document).ready(function(){
 
-		rowAdd();
+
 
 	});
